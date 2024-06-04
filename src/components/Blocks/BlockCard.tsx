afe649a9c3145e507125df7abfa8225e2aa72959
selankon <selankon@selankon.xyz>
2024-06-04 15:20:25 +0200

    Fix div cannot be descendant

--- src/components/Blocks/BlockCard.tsx
+++ src/components/Blocks/BlockCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Flex, Text } from '@chakra-ui/react'
+import { Box, Card, CardBody, Flex, Text } from '@chakra-ui/react'
 import { formatDistance } from 'date-fns'
 import { Trans } from 'react-i18next'
 import { ReducedTextAndCopy } from '~components/CopyBtn'
@@ -16,14 +16,14 @@ export const BlockCard = ({ height, time, proposer }: { height: number; time: st
               {formatDistance(date, new Date(), { addSuffix: true })}
             </Text>
           </Flex>
-          <Text fontSize={'sm'}>
+          <Box fontSize={'sm'}>
             <Flex gap={2} align={'center'}>
               <Trans i18nKey='blocks.proposer'>Proposer:</Trans>
               <ReducedTextAndCopy color={'textAccent1'} toCopy={proposer}>
                 {proposer}
               </ReducedTextAndCopy>
             </Flex>
-          </Text>
+          </Box>
         </Flex>
       </CardBody>
     </Card>
