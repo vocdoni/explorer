e3edbe89219da92bd4f1c2a95b55a9f78d8c00b8
Òscar Casajuana <elboletaire@gmail.com>
2024-06-05 10:31:32 +0200

    Add translation to the Loading component

--- src/router/SuspenseLoader.tsx
+++ src/router/SuspenseLoader.tsx
@@ -1,14 +1,15 @@
 import { Spinner, Square, Text } from '@chakra-ui/react'
 import { ReactNode, Suspense } from 'react'
+import { Trans } from 'react-i18next'
 
-export const Loading = () => {
-  return (
-    <Square centerContent size='full' minHeight='100vh'>
-      <Spinner size='sm' mr={3} />
-      <Text>Loading...</Text>
-    </Square>
-  )
-}
+export const Loading = () => (
+  <Square centerContent size='full' minHeight='100vh'>
+    <Spinner size='sm' mr={3} />
+    <Text>
+      <Trans i18nKey='loading'>Loading...</Trans>
+    </Text>
+  </Square>
+)
 
 export const SuspenseLoader = ({ children }: { children: ReactNode }) => (
   <Suspense fallback={<Loading />}>{children}</Suspense>
