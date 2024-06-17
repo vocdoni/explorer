f5ebb61387e88e53db9386d42494771038cf5530
selankon <selankon@selankon.xyz>
2024-06-17 15:24:02 +0200

    Use useDisclosure

--- src/layout/ShowRawButton.tsx
+++ src/layout/ShowRawButton.tsx
@@ -1,25 +1,21 @@
-import { Box, Button, ButtonProps, Code } from '@chakra-ui/react'
-import { useCallback, useState } from 'react'
-import { Trans } from 'react-i18next'
+import {Box, Button, ButtonProps, Code, useDisclosure} from '@chakra-ui/react'
+import {Trans} from 'react-i18next'
 
-const ShowRawButton = ({ obj, ...props }: { obj: object } & Omit<ButtonProps, 'onClick'>) => {
-  const [showRaw, setShowRaw] = useState(false)
+const ShowRawButton = ({obj, ...props}: { obj: object } & Omit<ButtonProps, 'onClick'>) => {
+  const {getDisclosureProps, getButtonProps} = useDisclosure()
+  const buttonProps = getButtonProps()
+  const disclosureProps = getDisclosureProps()
 
-  const toggleRaw = useCallback(() => {
-    setShowRaw(!showRaw)
-  }, [showRaw])
   return (
     <>
-      <Button onClick={toggleRaw} {...props}>
+      <Button {...buttonProps} {...props}>
         <Trans i18nKey={'raw_content'}>Raw content</Trans>
       </Button>
-      {showRaw && (
-        <Box wordBreak='break-all' whiteSpace='pre' color='white' borderRadius='md' maxW={'90vw'}>
-          <Code w={'full'} wordBreak='break-all' p='4' fontFamily='monospace' overflowX='auto'>
-            {JSON.stringify(obj, null, 2)}
-          </Code>
-        </Box>
-      )}
+      <Box {...disclosureProps} wordBreak='break-all' whiteSpace='pre' color='white' borderRadius='md' maxW={'90vw'}>
+        <Code w={'full'} wordBreak='break-all' p='4' fontFamily='monospace' overflowX='auto'>
+          {JSON.stringify(obj, null, 2)}
+        </Code>
+      </Box>
     </>
   )
 }
