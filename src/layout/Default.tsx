9112d97a6cd0726d8b62cc37a000cf9a430bb318
Òscar Casajuana <elboletaire@gmail.com>
2024-06-18 15:48:47 +0200

    Add scroll restoration component

--- src/layout/Default.tsx
+++ src/layout/Default.tsx
@@ -1,27 +1,24 @@
-import { Box, Grid } from '@chakra-ui/react'
-import { Outlet } from 'react-router-dom'
-import { TopBar } from '~components/TopBar'
-import { PropsWithChildren } from 'react'
+import { Box, BoxProps, Grid } from '@chakra-ui/react'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 import { Footer } from '~components/Footer'
+import { TopBar } from '~components/TopBar'
 
-const DefaultLayout = ({ children }: PropsWithChildren) => {
-  return (
-    <Box
-      p={{
-        base: '0 15px',
-        md: '0 15px',
-      }}
-      margin='0 auto'
-      maxW={'1152px'}
-      w='100%'
-    >
-      {children}
-    </Box>
-  )
-}
+const DefaultLayout = (props: BoxProps) => (
+  <Box
+    p={{
+      base: '0 15px',
+      md: '0 15px',
+    }}
+    margin='0 auto'
+    maxW={'1152px'}
+    w='100%'
+    {...props}
+  />
+)
 
 const Layout = () => (
   <Box minH='100vh' display='flex' flexDirection='column'>
+    <ScrollRestoration />
     <Grid p={3}>
       <TopBar />
       <DefaultLayout>
