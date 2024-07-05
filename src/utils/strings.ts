970e41d193db3feea2acb5ab48b17f98f919823e
selankon <selankon@selankon.xyz>
2024-07-05 09:46:35 +0200

    Search only valid partialId string

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -4,3 +4,11 @@ export const shortHex = (hex: string) => hex.substring(0, 6) + '...' + hex.subst
  * ucfirst makes the first letter of a string uppercase
  */
 export const ucfirst = (str: string, lng?: string | undefined) => str.charAt(0).toLocaleUpperCase(lng) + str.slice(1)
+
+/**
+ * Checks if a string is a valid hexadecimal with the correct length. Useful to check if processIds or orgsId are valid.
+ */
+export const isValidPartialProcessId = (str: string) => {
+  const hexRegex = /^[0-9A-Fa-f]+$/
+  return hexRegex.test(str) && str.length % 2 === 0
+}
