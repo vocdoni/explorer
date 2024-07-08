f90c6083450f05e1053f1f5d861f75943c73879e
selankon <selankon@selankon.xyz>
2024-07-08 09:54:40 +0200

    Fix typo

--- src/router/Error404.tsx
+++ src/router/Error404.tsx
@@ -3,7 +3,7 @@ import { Alert, AlertIcon } from '@chakra-ui/react'
 const Error404 = () => (
   <Alert status='warning'>
     <AlertIcon />
-    Looks like the content you were accessing coult not be found.
+    Looks like the content you were accessing could not not be found.
   </Alert>
 )
 
