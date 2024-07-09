d3b3943ce84a69f4d42201bad8f1112e61b68fcd
selankon <selankon@selankon.xyz>
2024-07-09 13:37:16 +0200

    Implements link mode on ReducedTextAndCopy

--- src/components/Layout/CopyButton.tsx
+++ src/components/Layout/CopyButton.tsx
@@ -1,8 +1,18 @@
-import { Button, ButtonProps, IconButton, Tooltip, useBreakpointValue, useClipboard } from '@chakra-ui/react'
+import {
+  Button,
+  ButtonProps,
+  Flex,
+  IconButton,
+  LinkProps,
+  Tooltip,
+  useBreakpointValue,
+  useClipboard,
+} from '@chakra-ui/react'
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { IoCheckmark, IoCopy } from 'react-icons/io5'
 import { shortHex } from '~utils/strings'
+import { Link as RouterLink } from 'react-router-dom'
 
 type ICopyButton = ButtonProps & {
   toCopy: string
@@ -46,30 +56,41 @@ export const CopyButtonIcon = withCopyLogic(IconButton)
  * It shows a text with a copy button. If not breakpoint is defined it uses default one: { base: true, sm: false },
  * @param reduced If it wants to be shown reduced independently of div size (for max length of 13)
  * @param breakPoint If it wants to be shown reduced on a specific breakpoint. If null it will show the entire text
+ * @param to if defined, the part of the text will be rendered as a link
  * @param children The text to be shown
  */
 export const ReducedTextAndCopy = ({
   reduced = false,
   breakPoint = { base: true, sm: false },
+  to,
   children = '',
   ...rest
 }: {
   reduced?: boolean
+  to?: string
   breakPoint?: Parameters<typeof useBreakpointValue>[0]
   children?: string
 } & ICopyButton) => {
+  let text = children
   // If it wants to be shown reduced or is small screen via the breakpoint
   if ((reduced && children.length > 13) || (breakPoint && useBreakpointValue(breakPoint))) {
-    return (
-      <CopyButton fontWeight={'normal'} h={0} fontSize={'xs'} p={0} pt={1} {...rest}>
-        {shortHex(children)}
-      </CopyButton>
-    )
+    text = shortHex(children)
   }
 
+  const isLink = (props: any): props is LinkProps => 'to' in props
+  if (isLink(rest)) {
+    return (
+      <Flex>
+        <Button as={RouterLink} to={to} variant={'text'} aria-label={children} {...rest}>
+          {text}
+        </Button>
+        <CopyButton {...rest} m={0} />
+      </Flex>
+    )
+  }
   return (
     <CopyButton fontWeight={'normal'} h={0} fontSize={'xs'} p={0} pt={1} {...rest}>
-      {children}
+      {text}
     </CopyButton>
   )
 }
