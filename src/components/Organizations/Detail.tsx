1de5e2cd8dba8e314e0cd4ef9b3253013cfeb655
selankon <selankon@selankon.xyz>
2024-07-19 09:37:56 +0200

    Add tabs to org detail

--- src/components/Organizations/Detail.tsx
+++ src/components/Organizations/Detail.tsx
@@ -1,14 +1,16 @@
-import { Box, Flex, Icon, Text, VStack } from '@chakra-ui/react'
+import { Box, Flex, Icon, Tab, TabList, TabPanel, TabPanels, Text, VStack } from '@chakra-ui/react'
 import { OrganizationDescription, OrganizationHeader, OrganizationName } from '@vocdoni/chakra-components'
 import { useOrganization } from '@vocdoni/react-providers'
 import { AccountData, ensure0x, PublishedElection } from '@vocdoni/sdk'
-import { Trans } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import { FaUserAlt } from 'react-icons/fa'
 import { useParams } from 'react-router-dom'
 import { ReducedTextAndCopy } from '~components/Layout/CopyButton'
+import { DetailsGrid, GridItemProps } from '~components/Layout/DetailsGrid'
 import { HeroHeaderLayout } from '~components/Layout/HeroHeaderLayout'
 import { LoadingCards } from '~components/Layout/Loading'
-import ShowRawButton from '~components/Layout/ShowRawButton'
+import { QueryParamsTabs } from '~components/Layout/QueryParamsTabs'
+import { RawContentBox } from '~components/Layout/ShowRawButton'
 import { RoutedPagination } from '~components/Pagination/Pagination'
 import { RoutedPaginationProvider } from '~components/Pagination/PaginationProvider'
 import { ElectionCard } from '~components/Process/Card'
@@ -28,51 +30,45 @@ const OrganizationDetail = () => {
     <>
       <HeroHeaderLayout header={<OrganizationHeader fallbackSrc={FallbackHeaderImg} />}>
         <VStack>
-          <OrganizationName fontSize='4xl' wordBreak='break-word' />
+          <OrganizationName fontSize='4xl' wordBreak='break-word' textAlign={'center'} />
           <ReducedTextAndCopy color={'textAccent1'} toCopy={id} fontWeight={'normal'} h={0} fontSize={'md'}>
             {id}
           </ReducedTextAndCopy>
-          <Flex
-            as={'a'}
-            target='blank'
-            href={`${AppBaseURL}/organization/${ensure0x(id)}`}
-            pt={4}
-            align={'end'}
-            gap={3}
-            color={'blueText'}
-          >
-            <Box>
-              <Icon as={FaUserAlt} boxSize={5} />
-            </Box>
-            <Box>
-              <Text fontSize='xl' verticalAlign='bottom'>
-                <Trans i18nKey={'organization.view_profile'}>(View profile)</Trans>
-              </Text>
-            </Box>
-          </Flex>
         </VStack>
       </HeroHeaderLayout>
-
-      <Flex align='start' gap={2} direction={'column'}>
-        {organization.account.description.default && (
-          <>
-            <Text fontSize='xl' color={'blueText'}>
-              <Trans i18nKey={'organization.description'}>Description</Trans>
-            </Text>
-            <OrganizationDescription />
-          </>
-        )}
-      </Flex>
-      <Text fontSize='xl' color={'blueText'}>
-        <Trans i18nKey={'organization.elections_list'}>Elections List:</Trans>
-      </Text>
-      <OrganizationElections org={organization} />
-      <ShowRawButton obj={organization} mt={4} />
+      <QueryParamsTabs isLazy>
+        <TabList display='flex' flexWrap='wrap'>
+          <Tab>
+            <Trans i18nKey={'process.tab_details'}>Details</Trans>
+          </Tab>
+          <Tab>
+            <Trans i18nKey={'organization.elections_list'}>Elections</Trans>
+          </Tab>
+          <Tab>
+            <Trans i18nKey={'raw'}>Raw</Trans>
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <OrgDetails org={organization} />
+          </TabPanel>
+          <TabPanel>
+            <OrganizationElections org={organization} />
+          </TabPanel>
+          <TabPanel>
+            <RawContentBox obj={organization} />
+          </TabPanel>
+        </TabPanels>
+      </QueryParamsTabs>
     </>
   )
 }
 
-const OrganizationElections = ({ org }: { org: AccountData }) => {
+interface OrgComponentProps {
+  org: AccountData
+}
+
+const OrganizationElections = ({ org }: OrgComponentProps) => {
   if (org.electionIndex === 0) {
     return (
       <Text>
@@ -94,7 +90,7 @@ const OrganizationElections = ({ org }: { org: AccountData }) => {
   )
 }
 
-const OrganizationElectionsList = ({ org }: { org: AccountData }) => {
+const OrganizationElectionsList = ({ org }: OrgComponentProps) => {
   const { page } = useParams()
 
   const { data: elections, isLoading } = useOrganizationElections({
@@ -116,6 +112,60 @@ const OrganizationElectionsList = ({ org }: { org: AccountData }) => {
         if (election instanceof PublishedElection) return <ElectionCard key={election.id} election={election} />
         return null
       })}
+    </Flex>
+  )
+}
+
+const OrgDetails = ({ org }: OrgComponentProps) => {
+  const { t } = useTranslation()
+
+  const details: GridItemProps[] = [
+    {
+      label: t('organization.nonce', { defaultValue: 'Nonce' }),
+      children: org.nonce,
+    },
+    {
+      label: t('organization.balance', { defaultValue: 'Balance' }),
+      children: org.balance,
+    },
+    {
+      label: t('organization.election_count', { defaultValue: 'Elections' }),
+      children: org.electionIndex,
+    },
+    {
+      label: t('organization.profile', { defaultValue: 'Profile' }),
+      children: (
+        <Flex
+          as={'a'}
+          target='blank'
+          href={`${AppBaseURL}/organization/${ensure0x(org.address)}`}
+          align={'end'}
+          gap={3}
+          color={'blueText'}
+        >
+          <Box>
+            <Icon as={FaUserAlt} boxSize={3} />
+          </Box>
+          <Box>
+            <Text verticalAlign='bottom'>
+              <Trans i18nKey={'organization.view_profile'}></Trans>
+            </Text>
+          </Box>
+        </Flex>
+      ),
+    },
+    ...(org.account.description.default
+      ? [
+          {
+            label: t('organization.description', { defaultValue: 'Description' }),
+            children: <OrganizationDescription />,
+          },
+        ]
+      : []),
+  ]
+  return (
+    <Flex align='start' gap={2} direction={'column'}>
+      <DetailsGrid details={details} />
     </Flex>
   )
 }
