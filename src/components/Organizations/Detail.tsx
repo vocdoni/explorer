87cc2c888115be6106d528fb7d07ea4f22a61530
selankon <selankon@selankon.xyz>
2024-07-23 11:48:09 +0200

    Implement transfers table

--- src/components/Organizations/Detail.tsx
+++ src/components/Organizations/Detail.tsx
@@ -1,10 +1,30 @@
-import { Box, Flex, Icon, Tab, TabList, TabPanel, TabPanels, Text, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Icon,
+  IconProps,
+  Link,
+  Tab,
+  Table,
+  TableContainer,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tooltip,
+  Tr,
+  VStack,
+} from '@chakra-ui/react'
 import { OrganizationDescription, OrganizationHeader, OrganizationName } from '@vocdoni/chakra-components'
 import { useOrganization } from '@vocdoni/react-providers'
 import { AccountData, ensure0x, PublishedElection } from '@vocdoni/sdk'
 import { Trans, useTranslation } from 'react-i18next'
 import { FaUserAlt } from 'react-icons/fa'
-import { useParams } from 'react-router-dom'
+import { generatePath, Link as RouterLink, useParams } from 'react-router-dom'
 import { ReducedTextAndCopy } from '~components/Layout/CopyButton'
 import { DetailsGrid, GridItemProps } from '~components/Layout/DetailsGrid'
 import { HeroHeaderLayout } from '~components/Layout/HeroHeaderLayout'
@@ -17,6 +37,8 @@ import { ElectionCard } from '~components/Process/Card'
 import { AppBaseURL, FallbackHeaderImg, PaginationItemsPerPage, RoutePath } from '~constants'
 import { useAccountTransfers, useAccountTransfersCount, useOrganizationElections } from '~queries/organizations'
 import { retryUnlessNotFound } from '~utils/queries'
+import { useDateFns } from '~i18n/use-date-fns'
+import { BiLogInCircle, BiLogOutCircle } from 'react-icons/bi'
 
 const OrganizationDetail = () => {
   const { organization } = useOrganization()
@@ -130,8 +152,30 @@ const OrganizationElectionsList = ({ org }: OrgComponentProps) => {
   )
 }
 
+const FromToIcon = ({ isIncoming, ...rest }: { isIncoming: boolean } & IconProps) => {
+  const { t } = useTranslation()
+
+  let icon = BiLogOutCircle
+  let color = 'textAccent2B'
+  let tooltip = t('organization.transfers.outgoing_tx', { defaultValue: 'Outgoing tx' })
+  if (isIncoming) {
+    icon = BiLogInCircle
+    color = 'accent1C'
+    tooltip = t('organization.transfers.incoming_tx', { defaultValue: 'Incoming tx' })
+  }
+
+  return (
+    <Tooltip label={tooltip}>
+      <span>
+        <Icon as={icon} color={color} {...rest} />
+      </span>
+    </Tooltip>
+  )
+}
+
 const AccountTransfers = ({ txCount, org }: { txCount: number | undefined } & OrgComponentProps) => {
   const { page } = useParams()
+  const { formatDistance } = useDateFns()
 
   const { data, isLoading } = useAccountTransfers({
     address: org.address,
@@ -145,7 +189,7 @@ const AccountTransfers = ({ txCount, org }: { txCount: number | undefined } & Or
   if (txCount && !(txCount > 0)) {
     return (
       <Text>
-        <Trans i18nKey={'organization.no_transfers'}>No transfers yet!</Trans>
+        <Trans i18nKey={'organization.transfers.no_transfers'}>No transfers yet!</Trans>
       </Text>
     )
   }
@@ -159,13 +203,86 @@ const AccountTransfers = ({ txCount, org }: { txCount: number | undefined } & Or
     mergedTransfers = [...data.transfers.received, ...data.transfers.sent]
     mergedTransfers.sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime())
     // Todo: put this out of the when https://github.com/vocdoni/vocdoni-sdk/pull/400 is merged
-    // Now is inside to infer the type properly
+    // Now is inside the if to infer the type properly
     return (
-      <Flex direction={'column'} gap={4}>
-        {mergedTransfers.map((transfers) => {
-          return transfers.toString()
-        })}
-      </Flex>
+      <Box overflow='auto' w='auto'>
+        <TableContainer>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>
+                  <Trans i18nKey={'organization.transfers.tx_hash'}>Tx Hash</Trans>
+                </Th>
+                <Th>
+                  <Trans i18nKey={'organization.transfers.block'}>Block</Trans>
+                </Th>
+                <Th>
+                  <Trans i18nKey={'organization.transfers.from_to'}>From/To</Trans>
+                </Th>
+                <Th>
+                  <Trans i18nKey={'organization.transfers.amount'}>Amount</Trans>
+                </Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {mergedTransfers.map((transfer) => {
+                const isIncoming = transfer.to === org.address
+                let fromToAddress = isIncoming ? transfer.from : transfer.to
+                return (
+                  <Tr>
+                    <Td>
+                      <Flex direction={'column'} align={'start'} gap={3}>
+                        <ReducedTextAndCopy
+                          breakPoint={{ base: true }}
+                          color={'textAccent1'}
+                          toCopy={transfer.txHash}
+                          fontWeight={'normal'}
+                          fontSize={'sm'}
+                          // todo: implement go to tx by its hash
+                        >
+                          {transfer.txHash}
+                        </ReducedTextAndCopy>
+
+                        <Text fontWeight={100} color={'lighterText'} fontSize={'sm'}>
+                          {formatDistance(new Date(transfer.timestamp), new Date())}
+                        </Text>
+                      </Flex>
+                    </Td>
+                    <Td>
+                      <Link
+                        as={RouterLink}
+                        to={generatePath(RoutePath.Block, { height: transfer.height.toString(), page: null })}
+                      >
+                        {transfer.height}
+                      </Link>
+                    </Td>
+                    <Td>
+                      <Flex align={'center'} gap={1}>
+                        <Box>
+                          <FromToIcon isIncoming={isIncoming} boxSize={5} />
+                        </Box>
+                        <ReducedTextAndCopy
+                          breakPoint={{ base: true }}
+                          color={'textAccent1'}
+                          toCopy={fromToAddress}
+                          fontWeight={'normal'}
+                          fontSize={'md'}
+                          p={1}
+                          h={8}
+                          to={generatePath(RoutePath.Organization, { pid: fromToAddress, page: null })}
+                        >
+                          {fromToAddress}
+                        </ReducedTextAndCopy>
+                      </Flex>
+                    </Td>
+                    <Td>{transfer.amount}</Td>
+                  </Tr>
+                )
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
     )
   }
   // Todo: put this out of the when https://github.com/vocdoni/vocdoni-sdk/pull/400 is merged
