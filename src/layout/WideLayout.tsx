490f56cb717101d64db94a10bdb30f8956890dd5
selankon <selankon@selankon.xyz>
2024-08-01 12:10:08 +0200

    Fix wide layout max width

--- src/layout/WideLayout.tsx
+++ src/layout/WideLayout.tsx
@@ -2,7 +2,7 @@ import { Box, BoxProps } from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom'
 
 const WideLayout = (props: BoxProps) => (
-  <Box px={{ base: 4, md: 6, xl: 12 }} w={'full'}>
+  <Box px={{ base: 4, md: 6, xl: 12 }} w={'full'} maxW={'1800px'} margin='0 auto'>
     <Outlet />
   </Box>
 )
