2453d82a23e076a9e83104a285d8bec8caf65e49
selankon <selankon@selankon.xyz>
2024-08-09 14:27:53 +0200

    Fix prettier

--- src/components/Stats/LatestBlocks.tsx
+++ src/components/Stats/LatestBlocks.tsx
@@ -31,7 +31,7 @@ export const LatestBlocks = () => {
   return (
     <Stack spacing={4}>
       {blocks.blocks.map((block, i) => (
-        <BlockCard key={i} block={block} compact/>
+        <BlockCard key={i} block={block} compact />
       ))}
       <Button as={RouterLink} to={generatePath(RoutePath.BlocksList)} bgColor='accent1' color={'white'}>
         <Trans i18nKey='stats.view_all_blocks'>View all blocks</Trans>
