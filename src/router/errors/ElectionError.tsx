d4d269e4377035c37c78d1a5fce1b062e76de7f5
selankon <selankon@selankon.xyz>
2024-08-23 09:33:07 +0200

    Improve invalid election page

--- src/router/errors/ElectionError.tsx
+++ src/router/errors/ElectionError.tsx
@@ -1,16 +1,39 @@
-import { Text } from '@chakra-ui/react'
-import { useRouteError } from 'react-router-dom'
+import { Flex, Heading, Text } from '@chakra-ui/react'
+import { useParams, useRouteError } from 'react-router-dom'
 import ShowRawButton from '~components/Layout/ShowRawButton'
 import RouteError from '~src/router/errors/RouteError'
+import { PublishedElection } from '@vocdoni/sdk'
+import { ReducedTextAndCopy } from '~components/Layout/CopyButton'
 
 export const ElectionError = () => {
   const error = useRouteError() as Error & { raw?: unknown; electionId: string }
+  const { pid }: { pid?: string } = useParams()
+
+  const electionId = pid || error.electionId || ''
+
+  let raw: any
+  let title = ''
+  if (error.raw) {
+    raw = error.raw
+    title = raw['metadata']['title']['default']
+  }
 
   return (
-    <>
+    <Flex direction={'column'} mt={{ base: '20px', lg: '40px' }} gap={6} wordBreak='break-all'>
+      {!!title && (
+        <Heading fontSize={'2xl'} fontWeight='bold'>
+          {title}
+        </Heading>
+      )}
+      {!!electionId && (
+        <Flex>
+          <ReducedTextAndCopy fontSize={'lg'} color={'textAccent1'} toCopy={electionId}>
+            {electionId}
+          </ReducedTextAndCopy>
+        </Flex>
+      )}
       <RouteError />
-      {!!error.electionId && <Text fontWeight='bold'>{error.electionId}</Text>}
-      {!!error.raw && <ShowRawButton obj={error.raw} />}
-    </>
+      {!!raw && <ShowRawButton obj={raw} />}
+    </Flex>
   )
 }
