1bad82a7401f365b28dafc6a943a6de892f239c3
Òscar Casajuana <elboletaire@gmail.com>
2024-08-26 17:35:57 +0200

    Move EnvelopeCard to its own component and remove unnecessary comments

--- src/components/Envelope/EnvelopeList.tsx
+++ src/components/Envelope/EnvelopeList.tsx
@@ -1,17 +1,13 @@
+import { Flex, Grid } from '@chakra-ui/react'
 import { useElection } from '@vocdoni/react-providers'
-import { PublishedElection, VoteSummary } from '@vocdoni/sdk'
-import { Trans, useTranslation } from 'react-i18next'
+import { PublishedElection } from '@vocdoni/sdk'
+import { useTranslation } from 'react-i18next'
 import { NoResultsError } from '~components/Layout/ContentError'
+import { LoadingCards } from '~components/Layout/Loading'
+import { Pagination } from '~components/Pagination/Pagination'
 import { PaginationProvider, usePagination } from '~components/Pagination/PaginationProvider'
 import { useElectionVotesList } from '~queries/processes'
-import { LoadingCards } from '~components/Layout/Loading'
-import { Box, Card, CardBody, CardHeader, Flex, Grid, Icon, Link, Text } from '@chakra-ui/react'
-import { Pagination } from '~components/Pagination/Pagination'
-import { BiEnvelope } from 'react-icons/bi'
-import { generatePath, Link as RouterLink } from 'react-router-dom'
-import { RoutePath } from '~constants'
-import LinkCard from '~components/Layout/LinkCard'
-import { BlockIconLink, TxIconLink } from '~components/Layout/IconLink'
+import { EnvelopeCard } from './EnvelopeCard'
 
 export const PaginatedEnvelopeList = () => {
   const { election: e } = useElection()
@@ -57,44 +53,9 @@ const EnvelopeList = () => {
         templateColumns={{ base: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)', lg: 'repeat(5, 1fr)' }}
         gap={4}
       >
-        {envelopes?.votes.map((envelope, i) => {
-          return <EnvelopeCard key={i} envelope={envelope} count={page * 10 + i + 1} />
-        })}
+        {envelopes?.votes.map((envelope, i) => <EnvelopeCard key={i} envelope={envelope} count={page * 10 + i + 1} />)}
       </Grid>
       <Pagination pagination={envelopes.pagination} />
     </Flex>
-  )
-}
-
-const EnvelopeCard = ({ envelope, count }: { envelope: VoteSummary; count: number }) => {
-  return (
-    <LinkCard to={generatePath(RoutePath.Envelope, { verifier: envelope.voteID, tab: null })} maxW='xs'>
-      <CardHeader>
-        <Flex justify={'space-between'}>
-          <Text fontWeight={'bold'}>
-            <Trans i18nKey={'envelopes.envelope_number'} num={count}>
-              Envelope nº {{ num: count }}
-            </Trans>
-          </Text>
-          <Box>
-            <Icon color={'lightText'} as={BiEnvelope} />
-          </Box>
-        </Flex>
-      </CardHeader>
-      <CardBody>
-        <Flex direction={'column'}>
-          {/*<Link*/}
-          {/*  as={RouterLink}*/}
-          {/*  to={generatePath(RoutePath.Block, { height: envelope.blockHeight.toString(), tab: null, page: null })}*/}
-          {/*>*/}
-          {/*  <Trans i18nKey={'envelopes.block'} height={envelope.blockHeight}>*/}
-          {/*    Block {{ height: envelope.blockHeight }}*/}
-          {/*  </Trans>*/}
-          {/*</Link>*/}
-          <BlockIconLink height={envelope.blockHeight} />
-          <TxIconLink block={envelope.blockHeight} index={envelope.transactionIndex} />
-        </Flex>
-      </CardBody>
-    </LinkCard>
   )
 }
