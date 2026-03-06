0c73c64c96ff22721be2cde3e58becafbf6d7109
Òscar Casajuana <elboletaire@gmail.com>
2026-03-06 11:52:58 +0100

    feat: add VOCHAIN_ENDPOINT env var for endpoint customization (#143)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ColorModeScript } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ClientProvider } from '@vocdoni/chakra-components'
 import { ExtendedSDKClient } from '@vocdoni/extended-sdk'
-import { EnvOptions } from '@vocdoni/sdk'
+import { ClientOptions, EnvOptions } from '@vocdoni/sdk'
 import { VocdoniEnvironment } from '~constants'
 import { RoutesProvider } from './router'
 
@@ -11,13 +11,19 @@ const queryClient = new QueryClient()
 export const App = () => {
   const environment = VocdoniEnvironment as EnvOptions
 
-  const sdkClient = new ExtendedSDKClient({
+  const options: ClientOptions = {
     env: environment,
-  })
+  }
+
+  if (import.meta.env.VOCHAIN_ENDPOINT) {
+    options.api_url = import.meta.env.VOCHAIN_ENDPOINT
+  }
+
+  const sdkClient = new ExtendedSDKClient(options)
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ClientProvider env={environment} client={sdkClient}>
+      <ClientProvider env={environment} client={sdkClient} options={options}>
         <RoutesProvider />
         <ColorModeScript />
       </ClientProvider>
